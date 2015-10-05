2305635922c055bb94bfa9bf7173a14fa1ee91e4
logavanc <logavanc@users.noreply.github.com>
2015-10-05 13:55:13 -0700

    Adding godoc documentation.

--- main.go
+++ main.go
@@ -16,6 +16,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software Foundation,
 // Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+// The 'shortpath' program is a small command line utility that returns a short
+// but unique string representing the current working directory.
 package main
 
 import (
