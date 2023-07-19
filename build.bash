fbf2b698cea9d84e8524584c2cf2a32d293820dc
VanCuren, Logan <logavanc@zednot.com>
2023-07-19 11:38:01 -0700

    Updated build/test scripts.

--- build.bash
+++ build.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Run from the script's directory.
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
 # Bash 'Strict Mode'
 # http://redsymbol.net/articles/unofficial-bash-strict-mode
@@ -21,7 +23,7 @@ go build -ldflags "-s -w" -o shortpath ./cli/shortpath/shortpath.go
 # Strip the binary and compress it with UPX.
 strip --strip-all ./shortpath
 upx --best ./shortpath
-ls -lah ./shortpath
+ls -lah "$(readlink -f ./shortpath)"
 
 # Check that the binary is statically linked.
 file ./shortpath | grep 'static.* linked'
--- test.bash
+++ test.bash
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-set -e
+# Run from the script's directory.
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
+
+# Bash 'Strict Mode'
+# http://redsymbol.net/articles/unofficial-bash-strict-mode
+# https://github.com/xwmx/bash-boilerplate#bash-strict-mode
+set -o nounset
+set -o errexit
+set -o pipefail
+IFS=$'\n\t'
 
 go test -cover ./...
